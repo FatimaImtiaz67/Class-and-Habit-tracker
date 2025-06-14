0200cacb374fc7d87fe22501b64a0131887a3815
Fatima Imtiaz <140704322+FatimaImtiaz67@users.noreply.github.com>
2025-06-14 15:46:11 +0500

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCYC6cMVmCegkXYUW2RJoIbD91vGyAiV1s",
-  authDomain: "rhythmix-27b7a.firebaseapp.com",
-  databaseURL: "https://rhythmix-27b7a-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "rhythmix-27b7a",
-  storageBucket: "rhythmix-27b7a.firebasestorage.app",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
   messagingSenderId: "76096707349",
-  appId: "1:76096707349:web:5297685dd47d304b37671c",
+  appId: "",
   measurementId: "G-59059ZJ9SH"
 };
 
